216c6e02235e40c905a08a327d7fe14e2783b4f3
GalileoGaliley <ThePursuer@mail.ru>
2020-11-14 14:56:01 +0300

    style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -47,7 +47,7 @@ export default function Header () {
                                         <Navbar.Toggle arial-controls='responsive-navbar-nav'/>
                                         <Navbar.Collapse id='responsive-navbar-nav'>
                                             <Nav className={'m-auto'}>
-                                                <Nav.Link href={'/'}>
+                                                <Nav.Link className={'link'} href={'/'}>
                                                     <svg width="1em" height="1em" viewBox="0 0 10 10"
                                                          className="bi bi-house-fill" fill="currentColor"
                                                          xmlns="http://www.w3.org/2000/svg">
@@ -65,7 +65,7 @@ export default function Header () {
                                                               d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4z"/>
                                                     </svg>
                                                 </button>
-                                                <Nav.Link href={'/Blog'}>
+                                                <Nav.Link className={'link'} href={'/Blog'}>
                                                     <svg width="1em" height="1em" viewBox="0 0 10 10"
                                                          className="bi bi-columns" fill="currentColor"
                                                          xmlns="http://www.w3.org/2000/svg">
